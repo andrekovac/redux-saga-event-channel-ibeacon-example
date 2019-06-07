eeb7e45a54b8579ef767d39b569f2bc6c4bccbdc
André Kovac <me@andrekovac.com>
2019-06-07 03:47:03 +0200

    Run react-native-kontaktio minimal example

--- App.js
+++ App.js
@@ -6,19 +6,39 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import Kontakt from 'react-native-kontaktio';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  NativeEventEmitter
+} from "react-native";
+import Kontakt, { KontaktModule } from "react-native-kontaktio";
+const { init, startDiscovery } = Kontakt;
+
+const kontaktEmitter = new NativeEventEmitter(KontaktModule);
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    init()
+      .then(() => startDiscovery())
+      .catch(error => alert("error", error));
+
+    // Add beacon listener
+    kontaktEmitter.addListener("didDiscoverDevices", ({ beacons }) => {
+      console.log("didDiscoverDevices", beacons);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -33,18 +53,18 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
