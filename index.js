6c39580e51a85dbfe20c1b689791179a292b93e8
André Kovac <me@andrekovac.com>
2019-06-23 13:39:05 +0200

    Move BeaconContainer into container folder and add App stubs with async/promise code for reference

--- index.js
+++ index.js
@@ -4,15 +4,11 @@
 import React from "react";
 import { AppRegistry } from "react-native";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
 
 import store from "./src/store";
 import App from "./src/components/App";
-import BeaconsContainer from "./src/components/BeaconsContainer";
+import BeaconContainer from "./src/container/BeaconContainer";
 import { name as appName } from "./app.json";
-
-// import reducer from "./reducers";
-// const store = createStore(reducer);
 
 // Don't show yellow warnings box. Messages still appear in console.
 // TODO: this is deprecated from v.0.52, use YellowBox.ignoreWarnings([...])
@@ -20,7 +16,7 @@ console.disableYellowBox = true;
 
 const Main = () => (
   <Provider store={store}>
-    <BeaconsContainer />
+    <BeaconContainer />
   </Provider>
 );
 
